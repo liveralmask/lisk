7d2a2a474a8e4b10b2ca2208c196e0b4e6f8dd15
liveralmask <liveralmask.lisk@gmail.com>
2014-10-13 14:24:16 +0900

    rails new lisk -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lisk::Application.config.secret_key_base = '5aa4bf50f6a3db9334c9262b34aa631273d31a5ffe5f523ae8090362a6434767a384b7e27686e178b9d126475859e44fef952eaa6aa7f93567cb441171e2eb59'
